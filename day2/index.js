df4275a2e30248a72184966bf110cf171aeec583
Ajay Kumar <ajaykumar89693479@gmail.com>
2024-11-24 16:46:29 +0530

    first commit

--- day2/index.js
+++ day2/index.js
@@ -1,4 +1,19 @@
-let a = 10;
-let b = 20;
-let sum = a+b;
-console.log(a+b);
+import figlet from "figlet";
+import colors from "colors";
+
+let data = "hello world";
+figlet(data, function (err, data) {
+    if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+    }
+    console.log(colors.rainbow(data));
+});
+
+
+console.log('hello'.green); // outputs green text
+console.log('i like cake and pies'.underline.red) // outputs red underlined text
+console.log('inverse the color'.inverse); // inverses the color
+console.log('OMG Rainbows!'.rainbow); // rainbow
+console.log('Run the trap'.trap); // Drops the bass
